f0f4af0a19fa9aff0011beb6596ba096c3be91b7
skylinestars <1140643368@qq.com>
2016-11-04 18:44:06 +0800

    零售模块 和 commom.css的增加

---
+++ WYS/src/app/modules/retail/content/retail.component.ts
@@ -0,0 +1,11 @@
+import { Component} from '@angular/core';
+
+@Component({
+   
+   selector: 'retail',
+   templateUrl: 'retail.component.html'
+})
+export class RetailComponent{
+    
+}
+
---
+++ WYS/src/app/modules/retail/retail.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+    
+    selector:'sales',
+    template:`
+    <div class="main">
+		<div class="main-left">
+			<h1>
+				<span style="color:rgb(164,74,140)">s</span>c
+			</h1>
+
+			<div class="list">
+				<div class="ultitle">零售</div>
+				
+			</div>
+			<div class="footer">欢迎使用</div>
+		</div>
+		<div class="main-right">
+			<router-outlet></router-outlet>
+		</div>
+    </div>
+    `
+})
+export class SalesComponent {
+
+}
