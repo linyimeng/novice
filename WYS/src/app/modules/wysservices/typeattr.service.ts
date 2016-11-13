a07e03fc23c9f1aca4b0d67b7c0ae022f33349ad
linyimeng <linyimeng88@gmail.com>
2016-11-13 09:26:31 +0800

    前端修改server层

--- WYS/src/app/modules/wysservices/typeattr.service.ts
+++ WYS/src/app/modules/wysservices/typeattr.service.ts
@@ -4,7 +4,7 @@ import { Http,Headers,RequestOptions } from '@angular/http';
 
 @Injectable()
 export class TypeattrService extends BaseService {
-    private GET_ATTR_LIST_URL = '/api/goods/typeattr/';
+    private GET_ATTR_LIST_URL = '/api/goods/attrs/';
 
     constructor(
         http:Http
@@ -12,7 +12,7 @@ export class TypeattrService extends BaseService {
 
     /** 获取单个类别的静态属性 */
     get_attr_list(attr_type:string,goodstype:number) {
-        let AttrTypeUrl = this.get_requesr_url(this.GET_ATTR_LIST_URL) + attr_type + '/'+goodstype + '.json';
+        let AttrTypeUrl = this.get_requesr_url(this.GET_ATTR_LIST_URL) +String(goodstype) + '/'+ attr_type + '.json';
         console.log(AttrTypeUrl);
         return this.http.get(AttrTypeUrl,this.get_auth_header())
                         .map(this.extractData)
